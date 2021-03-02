8a03f2f157861514bf554af065e95183fc3dda51
wsehl <widesehl@gmail.com>
2021-03-02 15:49:46 +0600

    add CORS protect

--- server/index.js
+++ server/index.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
+
+const whitelist = [process.env.LOCAL_URL, process.env.PRODUCTION_URL];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const router = require("./router");
 app.use("/api", router);
