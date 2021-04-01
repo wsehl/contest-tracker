ecfeb8c067e91ba3ee4a6785a1276270eaa7ab97
wsehl <widesehl@gmail.com>
2021-04-01 16:59:14 +0600

    move scripts to header

--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,5 @@
 </main>
-<script src="js/libs/font-awesome-5.15.2.min.js"></script>
-<script src="js/libs/bulma-toast-2.2.0.min.js"></script>
-<script src="js/libs/jquery-3.6.0.min.js"></script>
-<script src="js/libs/jquery-validate-1.19.3.min.js"></script>
-<script src="js/libs/swup-2.0.14.min.js"></script>
+
 <script src="js/index.js"></script>
 </body>
 
