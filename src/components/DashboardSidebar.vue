2e3cb1178cd605c6a0e8b4065a5c03a6c35ccae0
wsehl <widesehl@gmail.com>
2021-04-10 21:01:02 +0600

    layout changes

---
+++ src/components/DashboardSidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-drawer
+    class="left-navigation text-black aside"
+    style="border-right: 1px solid black"
+    show-if-above
+    v-model="left"
+    side="left"
+  >
+    <q-list>
+      <q-item to="/dashboard" active-class="tab-active" exact>
+        <q-item-section avatar>
+          <q-icon name="space_dashboard" class="text-dark" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Main</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item to="/dashboard/users" active-class="tab-active" exact>
+        <q-item-section avatar>
+          <q-icon name="people" class="text-dark" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Users</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-drawer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,57 +1,7 @@
 <template>
-  <q-layout view="lHh LpR lFf">
-    <q-header>
-      <q-toolbar>
-        <q-btn
-          @click="left = !left"
-          flat
-          round
-          dense
-          icon="menu"
-          class="q-mr-sm"
-        />
-        <q-toolbar-title></q-toolbar-title>
-        <q-btn flat round dense icon="home" to="/">
-          <q-tooltip :disable="$q.platform.is.mobile" v-close-popup
-            >Go to home page</q-tooltip
-          >
-        </q-btn>
-        <q-btn flat round dense icon="logout" @click="logout">
-          <q-tooltip :disable="$q.platform.is.mobile" v-close-popup
-            >Logout</q-tooltip
-          >
-        </q-btn>
-      </q-toolbar>
-    </q-header>
-    <q-drawer
-      class="left-navigation text-black aside"
-      style="border-right: 1px solid black"
-      show-if-above
-      v-model="left"
-      side="left"
-    >
-      <q-toolbar style="background: rgb(4 108 212); color: white;">
-        <q-toolbar-title>Dashboard</q-toolbar-title>
-      </q-toolbar>
-      <q-list>
-        <q-item to="/dashboard" active-class="tab-active" exact>
-          <q-item-section avatar>
-            <q-icon name="space_dashboard" class="text-dark" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Main</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item to="/dashboard/users" active-class="tab-active" exact>
-          <q-item-section avatar>
-            <q-icon name="people" class="text-dark" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Users</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
+  <q-layout view="hHh lpR fff">
+    <Navbar />
+    <DashboardSidebar />
     <q-page-container>
       <q-page class="row no-wrap">
         <div class="col">
@@ -67,17 +17,13 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+import DashboardSidebar from "@/components/DashboardSidebar.vue";
+
 export default {
-  data() {
-    return {
-      left: false,
-    };
-  },
-  methods: {
-    logout() {
-      this.$store.dispatch("logout");
-      this.$router.push("/");
-    },
+  components: {
+    Navbar,
+    DashboardSidebar,
   },
 };
 </script>
