434b84670ecf1e3c0ceabd07a24b6782f8b0ec39
wsehl <widesehl@gmail.com>
2021-04-29 22:31:13 +0600

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,37 +6,37 @@ axios.defaults.headers.common["Authorization"] = `Bearer ${store.state.token}`;
 const url = `${process.env.VUE_APP_BACKEND_URL}/api`;
 
 export default {
-  login(credentials) {
+  async login(credentials) {
     return axios
       .post(url + "/login", credentials)
       .then((response) => response.data);
   },
-  signup(credentials) {
+  async signup(credentials) {
     return axios
       .post(url + "/signup", credentials)
       .then((response) => response.data);
   },
-  getUsersTable() {
+  async getUsersTable() {
     return axios
       .get(url + "/dashboard/users")
       .then((response) => response.data);
   },
-  getOrganizationsTable() {
+  async getOrganizationsTable() {
     return axios
       .get(url + "/dashboard/organizations")
       .then((response) => response.data);
   },
-  getEventsTable() {
+  async getEventsTable() {
     return axios
       .get(url + "/dashboard/events")
       .then((response) => response.data);
   },
-  getTable(table) {
+  async getTable(table) {
     return axios
       .get(url + `/dashboard/${table}`)
       .then((response) => response.data);
   },
-  insertToTable(credentials, table) {
+  async insertToTable(credentials, table) {
     return axios
       .post(url + `/dashboard/${table}`, credentials)
       .then((response) => response.data);
