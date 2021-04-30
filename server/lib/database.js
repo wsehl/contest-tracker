2d8903ad21ffd98b57738f3132f5b8d81bad9ec9
wsehl <widesehl@gmail.com>
2021-04-30 09:53:20 +0600

    Update database.js

--- server/lib/database.js
+++ server/lib/database.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.info("Database successfully connected");
-  }
-});
+// db.connect((err) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.info("Database successfully connected");
+//   }
+// });
 
 module.exports = db;
