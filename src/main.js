436f5d839b62961c38d83ae1a8060fa0976d1523
wsehl <widesehl@gmail.com>
2021-04-30 11:44:10 +0600

    upd

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 import "./quasar";
+
+axios.defaults.headers.common["Authorization"] = `Bearer ${store.state.token}`;
 
 Vue.config.productionTip = false;
 
--- src/services/api.js
+++ src/services/api.js
@@ -3,22 +3,22 @@ import axios from "axios";
 const url = `${process.env.VUE_APP_BACKEND_URL}/api`;
 
 export default {
-  async login(credentials) {
+  login(credentials) {
     return axios
       .post(`${url}/login`, credentials)
       .then((response) => response.data);
   },
-  async signup(credentials) {
+  signup(credentials) {
     return axios
       .post(`${url}/signup`, credentials)
       .then((response) => response.data);
   },
-  async getTable(table) {
+  getTable(table) {
     return axios
       .get(`${url}/dashboard/${table}`)
       .then((response) => response.data);
   },
-  async insertToTable(credentials, table) {
+  insertToTable(credentials, table) {
     return axios
       .post(`${url}/dashboard/${table}`, credentials)
       .then((response) => response.data);
