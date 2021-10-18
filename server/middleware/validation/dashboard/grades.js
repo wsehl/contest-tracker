77893b8c69a4de996bf74230fdb44f2919deae8f
thaseyor <mikeston@mail.ru>
2021-10-18 23:35:54 +0600

    fix: typo

--- server/middleware/validation/dashboard/grades.js
+++ server/middleware/validation/dashboard/grades.js
@@ -4,7 +4,7 @@ const validateGrades = (req, res, next) => {
       msg: "Please enter a valid grade name",
     });
   }
-  if (!req.body.curator) {
+  if (!req.body.curator_id) {
     return res.status(400).send({
       msg: "Please enter a valid curator",
     });
