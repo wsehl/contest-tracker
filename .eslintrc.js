214f8e484ed0e5e5036c75a2bffd9096abca60e1
Alex Sehl <widesehl@gmail.com>
2021-12-10 22:57:05 +0600

    chore: fix packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-  root: true,
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  plugins: ["vue"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-unused-vars": "error",
+    "vue/multi-word-component-names": "off",
   },
 };
